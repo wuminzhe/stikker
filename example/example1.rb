934e5b321ae214256dfd80079ef8a416f9fd1cc1
wumz <wuminzhe@gmail.com>
2012-09-20 14:32:23 +0800

    change image composite way and add json description support

--- example/example1.rb
+++ example/example1.rb
@@ -1,7 +1,7 @@
 require '../stikker.rb'
 
 stikker = Stikker.new("http://photo.yupoo.com/ninjapan/Bp2axpm7/medish.jpg")
-stikker.add_text(230, 580, "& WHITE", {'fontsize'=>'33', 'fontcolor'=>'#ffffff'})
+stikker.add_text(230, 580, "& WHITE", {'fontsize'=>'33', 'fontcolor'=>'#ffffff', 'scale'=>'100%'})
 stikker.add_text(210, 580, "BLACK")
-stikker.add_image(50, 50, "../tmp/balloon.png")
+stikker.add_image(50, 50, "../tmp/qr.png", {'scale'=>'50%'})
 stikker.generate("./example1.jpg")
--- stikker.rb
+++ stikker.rb
@@ -24,27 +24,93 @@ class Stikker
     content = content.nil? ? '' : content.gsub("\"", "'")
     
     #
-    size =  config['size'].nil?           ? '200x'                    : config['size']
+    size =  config['size'].nil?           ? '560x'                    : config['size']
     font =  config['font'].nil?           ? "#{@dir}/fonts/msyh.ttf"  : config['font']
-    fontsize = config['fontsize'].nil?    ? '18'                      : config['fontsize']
+    fontsize = config['fontsize'].nil?    ? '21'                      : config['fontsize']
     fontcolor = config['fontcolor'].nil?  ? '#000000'                 : config['fontcolor']
     kerning = config['kerning'].nil?      ? '0'                       : config['kerning']
-    bgcolor = config['bgcolor'].nil?      ? 'none'                   : config['bgcolor']
+    bgcolor = config['bgcolor'].nil?      ? 'none'                    : config['bgcolor']
+    scale = config['scale'].nil?          ? '100%'                        : config['scale']
 
     #
-    @command << "-page +#{x}+#{y} \\\( -size #{size} -font '#{font}' -fill '#{fontcolor}' -pointsize #{fontsize} -kerning #{kerning} -background '#{bgcolor}' caption:\"#{content}\" \\\)"
+    #@command << "-page +#{x}+#{y} \\\( -size #{size} -font '#{font}' -fill '#{fontcolor}' -pointsize #{fontsize} -kerning #{kerning} -background '#{bgcolor}' caption:\"#{content}\" \\\)"
+    geometry = scale=='100%' ? "+#{x}+#{y}" : "#{scale}x#{scale}+#{x}+#{y}"
+    @command << "\\\( -size #{size} -font '#{font}' -fill '#{fontcolor}' -pointsize #{fontsize} -kerning #{kerning} -background '#{bgcolor}' caption:\"#{content}\" \\\) -geometry #{geometry} -composite"
   end
 
-  def add_image(x, y, image)
+  def add_image(x, y, image, config={})
     if image.start_with? 'http'
       image = download(image)
     end
-    @command << "-page +#{x}+#{y} '#{image}'"
+
+    scale = config['scale'].nil? ? '100%' : config['scale']
+    geometry = scale=='100%' ? "+#{x}+#{y}" : "#{scale}x#{scale}+#{x}+#{y}"
+    @command << "'#{image}' -geometry #{geometry} -composite" 
   end
 
-  def generate(file)
-    @command << "-background none -layers flatten '#{file}'"
+  #顺时针
+  def generate(file, rotate_degree=nil)
+    if not rotate_degree.nil? 
+      #@command << "-background none -layers flatten miff:- | convert -rotate #{rotate_degree} - '#{file}'"
+      @command << "miff:- | convert -rotate #{rotate_degree} - '#{file}'"
+    else
+      #@command << "-background none -layers flatten '#{file}'"
+      @command << "'#{file}'"
+    end
+    
+    puts @command.join(" \\\n")
     `#{@command.join(" ")}`
+  end
+
+  #不管怎么样，都生成jpg，imagemagick自动会进行图片格式转换
+  #result_file = "./stikker/tmp/result_#{Digest::SHA1.hexdigest(json)}.jpg" 
+  #如果同样的图片已经生成过了，那就不用第二次生成了
+  #return result_file if FileTest.exist?(result_file)
+  def Stikker.generate(json, file, rotate_degree=nil)
+    return nil if json.nil? or json.strip==''
+    return nil if json.include? '<html>'
+
+    #开始生成
+    covers_objs = JSON.parse(json)
+
+    #背景
+    background = covers_objs['background'].nil? ? 'http://kai7.cn/images/2368c013c52ad7be356da0ef02d5b30be1cbe936.png' : covers_objs["background"]
+    stikker = Stikker.new(background)
+
+    #文字
+    if(not covers_objs['texts'].nil?)
+      covers_objs['texts'].each do |t|
+        text = t['content'].gsub("　", "").gsub("\n", "").gsub("\r", "").gsub("'", "\\\’").gsub("\"", "\\\"").gsub(" ", "")
+        x = t['x']
+        y = t['y']
+        size = t['size']
+        color = t['color']
+        kerning = t['kerning']
+        #rotate = t['rotate']
+        width = t['width']
+        scale = t["scale"]
+
+        if not text.nil? and text.strip != ''
+          stikker.add_text(x, y, text, 'size'=>width, 'fontsize'=>size, 'fontcolor'=>color, 'kerning'=>kerning, 'scale'=>scale)
+        end
+      end
+    end
+    
+    #图片
+    if(not covers_objs['images'].nil?)
+      covers_objs['images'].each do |i|
+        src = i['src']
+        x = i['x']
+        y = i['y']
+        scale = i["scale"]
+
+        if not src.nil? and src.strip != ''
+          stikker.add_image(x, y, src, 'scale'=>scale)
+        end
+      end
+    end
+    
+    stikker.generate(file, rotate_degree)
   end
 
   private
